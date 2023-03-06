0f42f7ac594b91b0d1cced76bc029859a069e0ca
franrodriguez1993 <francojmprogramacion@gmail.com>
2023-03-06 10:22:04 -0300

    new projects

--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { LanguageContext } from "../context/LanguageProvider";
 import "../css/Projects.css";
 import projectsJSON from "../json/projects.json";
@@ -6,8 +6,6 @@ import projectsJSON from "../json/projects.json";
 import BackendCard from "../components/projectRoute/BackendCard";
 import FrontendCard from "../components/projectRoute/FrontendCard";
 const Projects = () => {
-  const [frontend, setFrontend] = useState(projectsJSON.frontend);
-  const [backend, setBackend] = useState(projectsJSON.backend);
   const { text } = useContext(LanguageContext);
   return (
     <div className="project-route">
@@ -15,13 +13,13 @@ const Projects = () => {
 
       <section className="backend-container">
         <h3 className="project-type_title">Backend</h3>
-        {backend.map((p) => (
+        {projectsJSON.backend.map((p) => (
           <BackendCard key={p.id} project={p} text={text} />
         ))}
       </section>
       <section className="frontend-container">
         <h3 className="project-type_title">Frontend</h3>
-        {frontend.map((p) => (
+        {projectsJSON.frontend.map((p) => (
           <FrontendCard key={p.id} project={p} text={text} />
         ))}
       </section>
